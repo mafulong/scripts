424b0d964310875864941cc82055f308a968d8b4
fulong ma <846897373@qq.com>
2021-08-23 14:09:42 +0800

    Update make_leetcode_cases.js

--- js/make_leetcode_cases.js
+++ js/make_leetcode_cases.js
@@ -1,21 +1,31 @@
 x = document.querySelector("#base_content > div.container > div > div > div:nth-child(3) > div > div.question-content.default-content")
-console.log(x)
+if (x==null){
+  x= document.querySelector("#question-detail-main-tabs > div.tab-pane__1SHj.css-12hreja-TabContent.e16udao5 > div > div.content__1Y2H > div")
+}
+// console.log(x)
 a = []
-b= []
 for (let node of x.childNodes) {
-  console.log(node.nodeName); // 显示集合中的所有节点
+//   console.log(node.nodeName); // 显示集合中的所有节点
   if (node.nodeName == "PRE"){
+      t = node.childNodes[1].data
+      t = t.replace(/\n/g, "")
+      t = t.replace(/ /g, "")
+      a.push(t)
+      t = node.childNodes[3].data
+      t = t.replace(/\n/g, "")
+      t = t.replace(/ /g, "")
+      a.push(t)
 //       console.log(node)
-      for (let pre of node.childNodes){
+//       for (let pre of node.childNodes){
 //           console.log(pre, pre.nodeName)
-          if (pre.nodeName == "#text"){
-               console.log('text:', pre)
-               t = pre.data
-               t = t.replace(/\n/g, "")
-               t = t.replace(/ /g, "")
-               a.push(t)
-          }
-      }
+// //           if (pre.nodeName == "#text"){
+// //                console.log('text:', pre)
+// //                t = pre.data
+// //                t = t.replace(/\n/g, "")
+// //                t = t.replace(/ /g, "")
+// //                a.push(t)
+// //           }
+//       }
   }
 }
 console.log(a)
@@ -23,11 +33,11 @@ n = a.length
 console.log(n)
 res = ""
 i = 0
-while (3*i<n){
+while (2*i<n){
 //     console.log(a[3*i], a[3*i+1], a[3*i+2])
-    in1 = a[3*i]
-    out1 = a[3*i+1]
-    t = "print(\"actual: \", f(" + in1 + "),\"should: \", "+out1+")"
+    in1 = a[2*i]
+    out1 = a[2*i+1]
+    t = "print(\"actual:\", f(" + in1 + "),\"should:\", "+out1+")"
     res = res+"\n"+ t
 //     console.log(t)
     i+=1
